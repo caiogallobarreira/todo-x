a7811f7cd33ce8c7b2dec090bb2ccccc1c6ea24f
caiogallobarreira <caiogallobarreira@gmail.com>
2024-10-14 12:50:13 -0300

    feat: suspense test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
-import { Task } from '@prisma/client';
 import { createTask, deleteTask, fetchAllTasks } from './actions';
 import AddTaskForm from "./components/add-task-form";
-import TaskCard from './components/task-card';
+import ListTasks from './components/list-tasks';
+import { Suspense } from 'react';
 
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store'
 export const revalidate = 1;
 
 export default async function Home() {
-  const tasks: Task[] = await fetchAllTasks();
 
   return (
     <div className="flex flex-col gap-4">
       <AddTaskForm handleCreate={createTask} />
 
       <div className="flex flex-col gap-2">
-        {tasks.map((task) => (
-          <TaskCard key={task.id} task={task} handleDelete={deleteTask}/>
-        ))}
+        <Suspense fallback={<h1>Carregando...</h1>}>
+          <ListTasks fetchTasks={fetchAllTasks} handleDelete={deleteTask} />
+        </Suspense>
       </div>
     </div>
   );
